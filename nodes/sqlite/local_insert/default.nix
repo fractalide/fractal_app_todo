6d5c171c16ca02131c727e04aebd67c0d66abfdb
Stewart Mackenzie <setori88@gmail.com>
2016-12-25 02:43:53 +0800

    Problem: subgraph doesn't support composite schema
    Solution: we now need to make explicit which contracts
    are used as initial messages (imsgs). We then work out
    all the deps and concatenate them together so the scheduler
    can understand how to use the imsg

--- nodes/sqlite/local_insert/default.nix
+++ nodes/sqlite/local_insert/default.nix
@@ -2,6 +2,7 @@
 
 subgraph {
   src = ./.;
+  edges = with edges; [ generic_text ];
   flowscript = with nodes; with edges; ''
   db_path => db_path insert(${sqlite_insert})
   '${generic_text}:(text="todos")' -> option insert()
